af17f87efaec89442c84f91b2d68fc0bef94047d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-10-07 18:19:55 +0800

    Delete frontend directory

--- frontend/notarize.sh
+++
@@ -1,1 +0,0 @@
-xcrun altool --notarize-app --primary-bundle-id "edu.stanford.magicmask" --username "lei@yangl1996.com" --password "@keychain:apple-store-connect" --file 'Magic Mask.zip'
--- frontend/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp frontend mask.app/Contents/MacOS/mask.app
-codesign -s Developer mask.app/Contents/libs/* --options runtime --entitlements entitlements.plist --timestamp -f 
-codesign -s Developer 'mask.app/Contents/MacOS/MagicMask' --options runtime --entitlements entitlements.plist --timestamp -f 
-codesign -s Developer mask.app --deep --options runtime --entitlements entitlements.plist --timestamp -f
